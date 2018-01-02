16acc8f3f03df768a36c3ef6f0cbaa7878f6aab3
vyoung <vyoung@forings.com>
2018-01-02 17:11:45 +0800

    test something

--- HDUSpider/HDUSpider/spiders/hdu.py
+++ HDUSpider/HDUSpider/spiders/hdu.py
@@ -9,19 +9,13 @@ class HduSpider(scrapy.Spider):
     offset = 1000
     start_urls = [base_url+str(offset)]
     def parse(self, response):
-        item = HduspiderItem()
-        item['title'] = response.xpath("//tr[4]/td/h1/text()").extract()[0]
-        item['description'] = response.xpath("//div[@class='panel_content'][1]/text()").extract()[0]
-        title_input = response.xpath("//td[@align='center']/div[@class='panel_title'][2]/text()").extract()[0]
-        if title_input == Input
-            item['inputs'] = response.xpath("//div[@class='panel_content'][2]/text()").extract()[0]
-        item['output'] = response.xpath("//div[@class='panel_content'][3]/text()").extract()[0]
-        item['sample_input'] = response.xpath("//div[@class='panel_content'][4]/pre/div/text()").extract()[0]
-        item['sample_output'] = response.xpath("//div[@class='panel_content'][5]/pre/div/text()").extract()[0]
-        item['time_limit'] = response.xpath("//td[@align='center']/font/b/span/text()").extract()[0].split(' ')[2]+'MS'
-        item['memory_limit'] = response.xpath("//td[@align='center']/font/b/span/text()").extract()[0].split(' ')[6] + 'K'
-        item['sourse'] = 'HDU'
-        print(item) 
+        #item = HduspiderItem()
+        #node_list = response.xpath("//td[@align='center']")
+        #title = node_list.xpath("//div[@class='panel_title']/text()").extract()
+        #content = node_list.xpath("//div[@class='panel_content']")
+        #print(title)
+        #print(item)
+        print("###############################")
         yield item
         if self.offset<6253:
             self.offset += 1
