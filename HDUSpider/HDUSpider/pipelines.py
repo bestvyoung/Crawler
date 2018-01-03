7a65beb1f0a20bb95c47e31c8b808e3233628472
vyoung <vyoung@forings.com>
2018-01-03 10:43:09 +0800

    problem items

--- HDUSpider/HDUSpider/pipelines.py
+++ HDUSpider/HDUSpider/pipelines.py
@@ -4,8 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import MySQLdb
 
 class HduspiderPipeline(object):
+    def __init__(self):
+        self.db = MySQLdb.connect("forings.com","root","123456",jol)
+        if self.db:
+            print("connect success")
     def process_item(self, item, spider):
+        cursor = db.cursor()
+        sql = "insert into problem(title,description,input,output,sample_input,sample_out,sourse,time_limit,memory_limit) values('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (item['title'],item['description'],item['Input'],item['Output'],item['SampleInput'],item['SampleOutput'],item['sourse'],item['time_limit'],item['memory_limit'])
         return item
--- HDUSpider/HDUSpider/spiders/hdu.py
+++ HDUSpider/HDUSpider/spiders/hdu.py
@@ -8,15 +8,31 @@ class HduSpider(scrapy.Spider):
     base_url = "http://acm.hdu.edu.cn/showproblem.php?pid="
     offset = 1000
     start_urls = [base_url+str(offset)]
+    def rep(self,string):
+        return string.replace(' ','')
     def parse(self, response):
-        #item = HduspiderItem()
-        #node_list = response.xpath("//td[@align='center']")
-        #title = node_list.xpath("//div[@class='panel_title']/text()").extract()
-        #content = node_list.xpath("//div[@class='panel_content']")
-        #print(title)
-        #print(item)
-        print("###############################")
-        yield item
+        item = HduspiderItem()
+        node_list = response.xpath("//td[@align='center']")
+        title = map(self.rep,node_list.xpath("//div[@class='panel_title']/text()").extract())
+        content = node_list.xpath("//div[@class='panel_content']")
+        item['time_limit'] = response.xpath("//td[@align='center']/font/b/span/text()").extract()[0].split(' ')[2]+'MS'
+        item['title'] = response.xpath("//tr[4]/td/h1/text()").extract()[0]
+        item['memory_limit'] = response.xpath("//td[@align='center']/font/b/span/text()").extract()[0].split(' ')[6] + 'K'
+        item['sourse'] = 'HDU'
+        item['description'] = response.xpath("//div[@class='panel_content'][1]/text()").extract()[0]
+
+        for i in range(len(title)):
+            if title[i] == 'Input':
+                item['Input'] = content[i].xpath("./text()").extract()[0]
+            if title[i] == 'Output':
+                item['Output'] = content[i].xpath("./text()").extract()[0]
+            if title[i] == 'SampleInput':
+                item['SampleInput'] = content[i].xpath("./pre/div/text()").extract()[0]
+            if title[i] == 'SampleOutput':
+                item['SampleOutput'] = content[i].xpath("./pre/div/text()").extract()[0]
+        print(item)
+        #print("###############################")
+        #yield item
         if self.offset<6253:
             self.offset += 1
             url = self.base_url + str(self.offset)
